8f51037c411a57796477115233b6f5320def69bd
Hridaya Prajapati <info.hridayaprajapati@gmail.com>
2025-05-30 12:19:39 +0545

    Add dummy KeyBoxOverlay
    Change-Id: I9986b5973826ab37c20596adab6797ade55e29cf

--- product.mk
+++ product.mk
@@ -3,4 +3,5 @@ PRODUCT_COPY_FILES += \
     vendor/extra/config-system_ext.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/overlay/config/config.xml
 
 PRODUCT_PACKAGES += \
-    CertifiedPropsOverlay
+    CertifiedPropsOverlay \
+    KeyBoxOverlay
